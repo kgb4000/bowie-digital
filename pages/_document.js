6d32e81099059280cd1c47294d61de20884d9e14
kgb <kgb@Kesters-iMac.fios-router.home>
2021-12-05 14:39:26 -0500

    Made changes to document.js page for css refresh

--- pages/_document.js
+++ pages/_document.js
@@ -1,55 +1,32 @@
-import Document, { Head, Main, Html, NextScript } from 'next/document'
-// Import styled components ServerStyleSheet
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    // Step 1: Create an instance of ServerStyleSheet
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-    // Step 2: Retrieve styles from components in the page
-    const page = renderPage(
-      (App) => (props) => sheet.collectStyles(<App {...props} />)
-    )
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
 
-    // Step 3: Extract the styles as <style> tags
-    const styleTags = sheet.getStyleElement()
-
-    // Step 4: Pass styleTags as a prop
-    return { ...page, styleTags }
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
-
-  //   render() {
-  //     return (
-  //       <Html>
-  //         <Head>
-  //           <link rel="preconnect" href="https://fonts.googleapis.com" />
-  //           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-  //           <link
-  //             href="https://fonts.googleapis.com/css2?family=Poppins:wght@900&display=swap"
-  //             rel="stylesheet"
-  //           />
-  //           <link
-  //             href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@900&display=swap"
-  //             rel="stylesheet"
-  //           />
-  //         </Head>
-  //         <body>
-  //           <Main />
-  //           <NextScript />
-  //         </body>
-  //       </Html>
-  //     )
-  //   }
-  // }
-
-  // import Document, { Html, Head, Main, NextScript } from 'next/document'
-
-  // class MyDocument extends Document {
-  //   static async getInitialProps(ctx) {
-  //     const initialProps = await Document.getInitialProps(ctx)
-  //     return { ...initialProps }
-  //   }
 
   render() {
     return (
