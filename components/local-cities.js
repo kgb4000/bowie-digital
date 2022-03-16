4e9fb42a99857a07feb965eb01e47a6d2dea3eaa
kgb <kgb@Kesters-iMac.fios-router.home>
2022-03-16 17:53:59 -0400

    Deleted local pages

--- components/local-cities.js
+++ components/local-cities.js
@@ -15,56 +15,20 @@ const LocalCities = () => {
             <a>Bowie</a>
           </Link>
           ,{' '}
-          <Link href="/calverton-maryland-local-seo-services">
-            <a>Calverton</a>
-          </Link>
-          ,{' '}
-          <Link href="/calvert-county-maryland-local-seo-services">
-            <a>Calvert County</a>
-          </Link>
-          ,{' '}
           <Link href="/capitol-heights-maryland-local-seo-services">
             <a>Capitol Heights</a>
-          </Link>
-          ,{' '}
-          <Link href="/cheverly-maryland-local-seo-services">
-            <a>Cheverly</a>
           </Link>
           ,{' '}
           <Link href="/clinton-maryland-local-seo-services">
             <a>Clinton</a>
           </Link>
           ,{' '}
-          <Link href="/college-park-maryland-local-seo-services">
-            <a>College Park</a>
-          </Link>
-          ,{' '}
           <Link href="/columbia-maryland-local-seo-services">
             <a>Columbia</a>
           </Link>
           ,{' '}
-          <Link href="/edgewater-maryland-local-seo-services">
-            <a>Edgewater</a>
-          </Link>
-          ,{' '}
-          <Link href="/forestville-maryland-local-seo-services">
-            <a>Forestville</a>
-          </Link>
-          ,{' '}
-          <Link href="/fort-washington-maryland-local-seo-services">
-            <a>Fort Washington</a>
-          </Link>
-          ,{' '}
-          <Link href="/glenn-dale-maryland-local-seo-services">
-            <a>Glenn Dale</a>
-          </Link>
-          ,{' '}
           <Link href="/greenbelt-maryland-local-seo-services">
             <a>Greenbelt</a>
-          </Link>
-          ,{' '}
-          <Link href="/howard-county-maryland-local-seo-services">
-            <a>Howard County</a>
           </Link>
           ,{' '}
           <Link href="/largo-maryland-local-seo-services">
@@ -75,29 +39,14 @@ const LocalCities = () => {
             <a>Laurel</a>
           </Link>
           ,{' '}
-          <Link href="/mitchellville-maryland-local-seo-services">
-            <a>Mitchellville</a>
-          </Link>
-          ,{' '}
           <Link href="/new-carrollton-md-local-seo-services">
             <a>New Carrollton</a>
-          </Link>
-          ,{' '}
-          <Link href="/prince-georges-county-md-local-seo-services">
-            <a>Prince George's County</a>
           </Link>
           ,{' '}
           <Link href="/temple-hills-maryland-local-seo-services">
             <a>Temple Hills</a>
           </Link>
           ,{' '}
-          <Link href="/south-laurel-maryland-local-seo-services">
-            <a>South Laurel</a>
-          </Link>
-          ,{' '}
-          <Link href="/upper-marlboro-md-local-seo-services">
-            <a>Upper Marlboro</a>
-          </Link>
         </p>
       </div>
     </>
